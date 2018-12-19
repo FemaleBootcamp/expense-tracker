4df8228137df551dfe9152cd239c35823e38f866
Simona <simonap11@yahoo.com>
2018-12-19 18:03:57 +0100

    Second Commit

--- database/migrations/2018_12_19_170050_create_expenses_table.php
+++ database/migrations/2018_12_19_170050_create_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateExpenseTable extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,8 @@ class CreateExpenseTable extends Migration
      */
     public function up()
     {
-        Schema::create('expense', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
+
             $table->increments('id');
             $table->string('title');
             $table->string('type');
@@ -34,6 +35,6 @@ class CreateExpenseTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('expense');
+        Schema::dropIfExists('expenses');
     }
 }
